86220feaa0c06b4af355e2d59c32c191094867c8
Sharik2468 <92333121+Sharik2468@users.noreply.github.com>
2025-04-10 08:49:53 +0300

    public

--- src/shared/api/index.ts
+++ src/shared/api/index.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import {data} from "react-router-dom";
 
-const apiUrl = 'https://localhost:7230/api'
+const apiUrl = 'https://fastbrasspage72.conveyor.cloud/api'
 const eventControllerUrl = `${apiUrl}/Holiday`
 const contractorControllerUrl = `${apiUrl}/Contractor`
 const authControllerUrl = `${apiUrl}/Auth`
