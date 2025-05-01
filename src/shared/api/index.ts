c013c9cc80f0846b9b37b56e921322d33e91323a
Sharik2468 <92333121+Sharik2468@users.noreply.github.com>
2025-05-01 21:21:10 +0300

    replace api

--- src/shared/api/index.ts
+++ src/shared/api/index.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const apiUrl = 'https://localhost:7230/api'
+const apiUrl = 'https://holidayplanningapi-production.up.railway.app/api'
 const eventControllerUrl = `${apiUrl}/Holiday`
 const contractorControllerUrl = `${apiUrl}/Contractor`
 const authControllerUrl = `${apiUrl}/Auth`
